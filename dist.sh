2deb790626d8ea18940064b6b8e62398712e103f
Bernhard Walle <bernhard@bwalle.de>
2009-08-30 08:46:03 +0200

    Exclude 'dist.sh' from tarball

--- dist.sh
+++ dist.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 release=`grep PACKAGE_VERSION CMakeLists.txt |head -1|sed -e 's/.*"\(.*\)\".*/\1/g'`
-hg archive -t tbz2 pxe-kexec-${release}.tar.bz2
+hg archive -X dist.sh -t tbz2 pxe-kexec-${release}.tar.bz2
 
