3273715ed66469c3c27467e93621261c19f2cbf9
Bernhard Walle <bernhard@bwalle.de>
2009-09-06 13:04:11 +0200

    Add RHEL/CentOS >= 5.3 to the whitelist

--- src/pxekexec.cc
+++ src/pxekexec.cc
@@ -365,6 +365,9 @@ bool PxeKexec::checkEnv()
         bool suitable_dist = false;
         if ((dist == "Ubuntu"       && rpmvercmp(detected_version, "9.04") >= 0)  ||
             (dist == "openSUSE"     && rpmvercmp(detected_version, "11.0") >= 0)  ||
+            ((dist == "CentOS" ||
+              startsWith(dist, "Red Hat Enterprise Linux"))
+                                    && rpmvercmp(detected_version, "5.3") >= 0) ||
             (type == LinuxDistDetector::DT_ARCH)) {
             suitable_dist = true;
         }
