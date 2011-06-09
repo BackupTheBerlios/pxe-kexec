e308935c12c6cf7e7086f9ede5034dfe955c8587
Bernhard Walle <bernhard@bwalle.de>
2011-06-09 06:36:35 +0200

    Change dist.sh to use hg
    Signed-off-by: Bernhard Walle <bernhard@bwalle.de>

--- dist.sh
+++ dist.sh
@@ -5,5 +5,5 @@ NAME=pxe-kexec
 
 VERSION=`grep PACKAGE_VERSION CMakeLists.txt |head -1|sed -e 's/.*"\(.*\)\".*/\1/g'`
 ARCHIVE=$NAME-$VERSION.tar.bz2
-git archive --format=tar --prefix=$NAME-$VERSION/ master | bzip2 > $ARCHIVE
+hg archive -X dist.sh -t tbz2 $NAME-${VERSION}.tar.bz2
 echo "Archive '$ARCHIVE' ready!"
